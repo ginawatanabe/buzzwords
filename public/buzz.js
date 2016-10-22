5de45a95d45e95ece5b70d99973390548a123145
Gina W <watanabegina26@gmail.com>
2016-10-22 13:35:20 -1000

    Added routes and scoring.

--- public/buzz.js
+++ public/buzz.js
@@ -5,8 +5,19 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }))
+var collection = [];
+var score = 0;
 
-var buzzwords = [];
+//Buzzword Object Constructor
+function buzzword(buzzWord, points, heard) {
+  this.buzzWord = buzzWord;
+  this.points = points;
+  this.heard = heard;
+}
+
+var firstBuzz = new buzzword("um", 100, false);
+
+collection.push(firstBuzz);
 
 app.listen(3000);
 
@@ -15,29 +26,95 @@ app.get('/', function (req, res) {
 });
 
 app.get('/buzzwords', function (req, res) {
-  res.send({
-    buzzwords: ["um", "er", "like"]
-  })
+  res.send(buzzwords);
 });
 
-app.post('/buzzword', function (req, res) {
-  res.json({
-    buzzwords: "Hello",
-    points: "5"
-  })
+app.post('/buzzwords', function (req, res) {
+  //store req.body in variable buzz
+    var buzz = req.body;
 
-  var newBuzz = req.body;
+  if (buzz.heard === 'true') {
+    score += Number(buzz.points);
+    console.log(score);
+  } else {
+    score = score;
+    console.log(score);
+  }
 
-  buzzwords.push(newBuzz);
 
-  res.send({success: true, buzzwords: newBuzz});
+//if it can push the new buzzword object in the array, return success
+  if (collection.push(buzz)) {
+    console.log(collection);
+    return res.send({"success": true});
+  } else {
+    return res.send({"success": false});
+  }
+})
+
+app.put('/buzzwords', function (req,res) {
+  //variables for storing req.body and things to be updated
+    var buzzChange = req.body;
+    var buzzy = buzzChange.buzzWord;
+    var newPoints = buzzChange.points;
+    var heardIt = buzzChange.heard;
+
+  if (heardIt === 'true') {
+    score += Number(buzzChange.points);
+    console.log(score);
+  } else {
+    score = score;
+    console.log(score);
+  }
+
+//change buzzword's points and heard boolean
+//lookup loop from stackoverflow
+  var lookup = {};
+  for (i = 0; i<collection.length; i++) {
+    lookup[collection[i].buzzWord] = collection[i];
+  }
+
+  var changedPoints = lookup[buzzy].points = newPoints;
+  var changedHeard = lookup[buzzy].heard = heardIt;
+
+  if (changedPoints && changedHeard) {
+    console.log(collection);
+    return res.send({
+      "success": true,
+      newScore: newPoints
+    });
+  } else {
+    return res.send({"success": false});
+  }
 
 })
-//
-// app.put('/buzzword', function (req,res) {
-//   res.json(
-//     "buzzword":
-//   )
-// })
-//
-// app.put('/buzzword')
+
+app.delete('/buzzwords', function(req, res) {
+  var buzzChange = req.body;
+  var buzzy = buzzChange.buzzWord;
+
+  var lookup = {};
+  for (i = 0; i<collection.length; i++) {
+    lookup[collection[i].buzzWord] = collection[i];
+  }
+
+  if (buzzChange.heard === 'true') {
+    score = score - Number(buzzChange.points);
+    console.log(score);
+  } else {
+    score = score;
+    console.log(score);
+  }
+
+//remove buzzword from collection
+  for (i=0; i<collection.length; i++) {
+    var obj = collection[i];
+    if (collection[i].buzzWord === buzzy) {
+      collection.splice(i, 1);
+      console.log(collection);
+      return res.send({"success":true});
+    } else {
+      return res.send({"success":false})
+    }
+  }
+
+})
