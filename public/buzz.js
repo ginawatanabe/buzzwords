0dc0ece79d63a0109d6abee528b8abf24d6a0a95
Gina W <watanabegina26@gmail.com>
2016-10-22 14:25:21 -1000

    Added comments for code.

--- public/buzz.js
+++ public/buzz.js
@@ -5,34 +5,42 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }))
+
+//Collection of buzzword objects.
 var collection = [];
+
+//Score.
 var score = 0;
 
-//Buzzword Object Constructor
+//Buzzword Object Constructor.
 function buzzword(buzzWord, points, heard) {
   this.buzzWord = buzzWord;
   this.points = points;
   this.heard = heard;
 }
 
+//Initial Buzzword Object.
 var firstBuzz = new buzzword("um", 100, false);
-
 collection.push(firstBuzz);
 
+//Listen on Port 3000.
 app.listen(3000);
 
+//Get route to render index file.
 app.get('/', function (req, res) {
   res.render('/index');
 });
 
+//Get route to send the initial buzzword.
 app.get('/buzzwords', function (req, res) {
-  res.send(buzzwords);
+  res.send(firstBuzz);
 });
 
+//Post route to post new buzzwords.
 app.post('/buzzwords', function (req, res) {
-  //store req.body in variable buzz
-    var buzz = req.body;
+  var buzz = req.body;
 
+  //Update score.
   if (buzz.heard === 'true') {
     score += Number(buzz.points);
     console.log(score);
@@ -42,7 +50,7 @@ app.post('/buzzwords', function (req, res) {
   }
 
 
-//if it can push the new buzzword object in the array, return success
+  //If it can push the new buzzword object into the array, return success.
   if (collection.push(buzz)) {
     console.log(collection);
     return res.send({"success": true});
@@ -51,23 +59,25 @@ app.post('/buzzwords', function (req, res) {
   }
 })
 
+//Put route to update buzzwords.
 app.put('/buzzwords', function (req,res) {
-  //variables for storing req.body and things to be updated
-    var buzzChange = req.body;
-    var buzzy = buzzChange.buzzWord;
-    var newPoints = buzzChange.points;
-    var heardIt = buzzChange.heard;
+  //Variables for storing req.body and things to be updated.
+  var buzzChange = req.body;
+  var buzzy = buzzChange.buzzWord;
+  var newPoints = buzzChange.points;
+  var heardIt = buzzChange.heard;
 
-  // if (heardIt === 'true') {
-  //   score += Number(buzzChange.points) - lookup[buzzy].points;
-  //   console.log(score);
-  // } else {
-  //   score = score;
-  //   console.log(score);
-  // }
+  //Update score.
+  if (heardIt === 'true') {
+    score += Number(buzzChange.points);
+    console.log(score);
+  } else {
+    score = score;
+    console.log(score);
+  }
 
-//change buzzword's points and heard boolean
-//lookup loop from stackoverflow
+  //Change buzzword's points and heard boolean
+  //Lookup loop from stackoverflow
   var lookup = {};
   for (i = 0; i<collection.length; i++) {
     lookup[collection[i].buzzWord] = collection[i];
@@ -76,6 +86,7 @@ app.put('/buzzwords', function (req,res) {
   var changedPoints = lookup[buzzy].points = newPoints;
   var changedHeard = lookup[buzzy].heard = heardIt;
 
+  //If successfully changed, send success and points.
   if (changedPoints && changedHeard) {
     console.log(collection);
     return res.send({
@@ -88,42 +99,37 @@ app.put('/buzzwords', function (req,res) {
 
 })
 
+//Delete route to delete buzzword objects.
 app.delete('/buzzwords', function(req, res) {
   var buzzChange = req.body;
   var buzzy = buzzChange.buzzWord;
+  console.log(score);
 
   var lookup = {};
   for (i = 0; i<collection.length; i++) {
     lookup[collection[i].buzzWord] = collection[i];
   }
 
-//update score
-  // if (buzzChange.heard === 'true') {
-  //   score = score - Number(buzzChange.points);
-  //   console.log(score);
-  // } else {
-  //   score = score;
-  //   console.log(score);
-  // }
-
-//remove buzzword from collection
+  //Remove buzzword from collection.
   for (i=0; i<collection.length; i++) {
     var obj = collection[i];
-    if (collection[i].buzzWord === buzzy) {
+    if (obj.buzzWord === buzzy) {
       collection.splice(i, 1);
       console.log(collection);
       return res.send({"success":true});
     } else {
-      return res.send({"success":false})
+      // return res.send({"success":false});
     }
   }
 
 })
 
+//Post route to reset buzzword collection and reset score to 0.
 app.post('/reset', function(req,res) {
   score = 0;
   console.log(score);
 
+  //If there has been a successful reset using splice, return success. 
   if (collection.splice(0, collection.length)) {
     console.log(collection);
     return res.send({"success": true});
